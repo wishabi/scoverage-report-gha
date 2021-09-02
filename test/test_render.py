ec0b3b0797e82838d07e06126bbb094336499510
julio <julio.vilela@flipp.com>
2021-09-02 15:45:35 -0400

    DAT-848 Refactor code to use data classes, and split comment into 2 tables

--- test/test_render.py
+++ test/test_render.py
@@ -1,53 +1,54 @@
 import unittest
-import os
-from main.render import render_pr_comment
 from .config import config
+from main.models import ReportCoverage, CoverageEntry
+from main.render import render_pr_comment
 
 
-class TestProcess(unittest.TestCase):
-    TEST_COVERAGE_FILE = os.path.join(os.path.dirname(__file__),
-                                      'resources',
-                                      'scoverage.xml')
+class TestRender(unittest.TestCase):
 
     ICON_MAPPINGS = config['render']['icon_mappings']
 
     def test_render_pr_comment_pass_threshold(self):
-        results = {
-            'statement_coverage': 0.75,
-            'com.app.pk1': 0.9,
-            'com.app.pk2': 0.6
-        }
-        thresholds = {
-            'statement_coverage': 0.20
-        }
-        comment = render_pr_comment(results, self.ICON_MAPPINGS, thresholds)
+        results = ReportCoverage(
+            overall=CoverageEntry('statement_coverage', 0.75, threshold=0.20, is_package=False),
+            packages=[
+                CoverageEntry('com.app.pk1', 0.9, is_package=True),
+                CoverageEntry('com.app.pk2', 0.6, is_package=True)
+            ]
+        )
+        comment = render_pr_comment(results, self.ICON_MAPPINGS)
         expected_comment = self.__build_comment([
-            '|Code Coverage|%|Status|',
+            '|Overall|%|Status|',
             '|:-|:-:|:-:|',
             '|Statement Coverage|75.0|:white_check_mark:|',
+            '',
+            '|Package Coverage|%|Status|',
+            '|:-|:-:|:-:|',
             '|com.app.pk1|90.0||',
             '|com.app.pk2|60.0||'
         ])
-        self.assertEqual(comment, expected_comment)
+        self.assertEqual(comment.msg, expected_comment)
 
     def test_render_pr_comment_fail_threshold(self):
-        results = {
-            'statement_coverage': 0.60,
-            'com.app.pk1': 0.7,
-            'com.app.pk2': 0.5
-        }
-        thresholds = {
-            'statement_coverage': 0.90
-        }
-        comment = render_pr_comment(results, self.ICON_MAPPINGS, thresholds)
+        results = ReportCoverage(
+            overall=CoverageEntry('statement_coverage', 0.60, threshold=0.90, is_package=False),
+            packages=[
+                CoverageEntry('com.app.pk1', 0.7, is_package=True),
+                CoverageEntry('com.app.pk2', 0.5, is_package=True)
+            ]
+        )
+        comment = render_pr_comment(results, self.ICON_MAPPINGS)
         expected_comment = self.__build_comment([
-            '|Code Coverage|%|Status|',
+            '|Overall|%|Status|',
             '|:-|:-:|:-:|',
             '|Statement Coverage|60.0|:x:|',
+            '',
+            '|Package Coverage|%|Status|',
+            '|:-|:-:|:-:|',
             '|com.app.pk1|70.0||',
             '|com.app.pk2|50.0||'
         ])
-        self.assertEqual(comment, expected_comment)
+        self.assertEqual(comment.msg, expected_comment)
 
     @staticmethod
     def __build_comment(rows):
